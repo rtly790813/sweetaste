cba7b5b3d1adda2914b97b3c9e7679122d64908f
Lucy <rtly790813@gmail.com>
2018-09-27 09:27:29 +0800

    add images

--- gulpfile.js
+++ gulpfile.js
@@ -6,27 +6,27 @@ gulp.task('sass', function() {
     var plugins = [
         autoprefixer({ browsers: ['last 3 version', '> 5%'] }),
     ];
-    return gulp.src('./src/assets/sass/**/*.scss')
+    return gulp.src('./assets/sass/**/*.scss')
         .pipe($.plumber())
         .pipe($.sass({ outputStyle: 'compressed' }).on('error', $.sass.logError))
         .pipe($.postcss(plugins))
-        .pipe(gulp.dest('./src/public/css'));
+        .pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('babel', () =>
-    gulp.src('./src/assets/javascript/**/*.js')
+    gulp.src('./assets/javascript/**/*.js')
     .pipe($.sourcemaps.init())
     .pipe($.babel({
         presets: ['@babel/env']
     }))
     .pipe($.concat('all.js'))
     .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest('./src/public/javascript'))
+    .pipe(gulp.dest('./public/javascript'))
 );
 
 gulp.task('watch', function() {
-    gulp.watch('./src/assets/sass/**/*.scss', ['sass']);
-    gulp.watch('./src/assets/javascript/**/*.js', ['babel']);
+    gulp.watch('./assets/sass/**/*.scss', ['sass']);
+    gulp.watch('./assets/javascript/**/*.js', ['babel']);
 });
 
 gulp.task('default', ['sass', 'babel', 'watch'])
